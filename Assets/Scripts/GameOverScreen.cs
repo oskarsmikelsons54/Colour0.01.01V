fc6a2765668ee2d6d2ba0fd4cc1621b8dd2b89c1
oskarsmikelsons54 <oskarsmikelsons54@gmail.com>
2026-05-22 16:36:22 +0300

    LevelSistema cita
    Katra zona savs scene

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -5,21 +5,34 @@ public class GameOverScreen : MonoBehaviour
 {
     public GameObject panel;
 
+    private void Start()
+    {
+        // Hide panel at start
+        panel.SetActive(false);
+    }
+
     public void Show()
     {
+        // DEBUG MESSAGE
+        Debug.Log("Game Over Screen Showing");
+
         panel.SetActive(true);
         Time.timeScale = 0f;
     }
 
     public void Restart()
     {
+        Debug.Log("Restart Button Pressed");
+
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void MainMenu()
     {
-        Time.timeScale = 1f; // 👈 IMPORTANT
+        Debug.Log("Main Menu Button Pressed");
+
+        Time.timeScale = 1f;
         SceneManager.LoadScene(0);
     }
 }
